580cf5c24c15d7eeb890ebcd46d3f31576246514
roobeedeedada <bulina123@gmail.com>
2013-04-11 20:18:32 +0100

    Use the right event for hovering (mouseover)
    jQuery version functions inside its own scope so it doesn't conflict with other libraries

--- jquery-tab-switcher.js
+++ jquery-tab-switcher.js
@@ -4,80 +4,85 @@
  * 	feel free to minify
  */
 
-KISCODE = {};
+(function($) {  
 
-KISCODE.TabSwitcher = function( config ) {
-	this.currentIdx = config.defaultTab ? config.defaultTab : 0;
-	this.circular 	= config.circular;
-	this.$container = $( config.container );
-	this.$prev 		= $( config.prevButton );
-	this.$next 		= $( config.nextButton );
-	this.$buttons 	= this.$container.find('.clickable');
-	this.$tabs 		= this.$container.find('.tab-content');
-	this.animate 	= config.animate;
-	this.eventType 	= $.inArray( config.eventType, [ 'hover', 'click' ] ) > -1 ? config.eventType : 'click';
-	this.stateful 	= config.allowCookies;
-	this.kiscookie 	= 'kistabs' + $.inArray( this, window.kismet.components );
-	
-	// Check if the jQuery.cookie is available
-	if( this.stateful && $.cookie && $.cookie( this.kiscookie ) ) {
-		this.currentIdx = parseInt( $.cookie( this.kiscookie ) );
-	}
+	KISCODE = {};
 
-	this.$tabs.eq( this.currentIdx ).addClass('selected');
-	this.switchTab( this.currentIdx );	
-
-	var that = this;
-	this.$buttons.each( function() {
-		$( this ).on( that.eventType, function( e ) {
-			var clickedIdx = $.inArray( this, that.$buttons );
-			if( clickedIdx !== that.currentIdx ) {
-				that.switchTab( clickedIdx );
-				e.preventDefault();	
-			}
-		} );
-	} );
-
-	this.$prev.on( that.eventType, function( e ) {
-		if( that.currentIdx > 0 ) {
-			that.switchTab( that.currentIdx - 1 );
-		} else if( that.circular ) {
-			that.switchTab( that.$tabs.length - 1 );
+	KISCODE.TabSwitcher = function( config ) {
+		this.currentIdx = config.defaultTab ? config.defaultTab : 0;
+		this.circular 	= config.circular;
+		this.$container = $( config.container );
+		this.$prev 		= $( config.prevButton );
+		this.$next 		= $( config.nextButton );
+		this.$buttons 	= this.$container.find('.clickable');
+		this.$tabs 		= this.$container.find('.tab-content');
+		this.animate 	= config.animate;
+		this.eventType 	= $.inArray( config.eventType, [ 'mouseover', 'click' ] ) > -1 ? config.eventType : 'click';
+		this.stateful 	= config.allowCookies;
+		this.name 		= config.name;
+		this.kiscookie 	= config.name + '-kistabs';
+		
+		// Check if the jQuery.cookie is available
+		if( this.stateful && this.name && $.cookie && $.cookie( this.kiscookie ) ) {
+			this.currentIdx = parseInt( $.cookie( this.kiscookie ) );
 		}
-		e.preventDefault();
-	} );
 
-	this.$next.on( that.eventType, function( e ) {
-		if( that.currentIdx < that.$tabs.length - 1 ) {
-			that.switchTab( that.currentIdx + 1 );
-		} else if( that.circular ) {
-			that.switchTab( 0 );
-		}
-		e.preventDefault();
-	} );
-};
+		this.$tabs.eq( this.currentIdx ).addClass('selected');
+		this.switchTab( this.currentIdx );	
 
-KISCODE.TabSwitcher.prototype.switchTab = function( index ) {
-	// Handle tab highlighting
-	this.$buttons.eq( this.currentIdx ).removeClass( 'highlight' );
-	this.$buttons.eq( index ).addClass( 'highlight' );
-
-	// Handle content switching
-	if( this.animate ) {
 		var that = this;
-		this.$tabs.eq( that.currentIdx ).siblings('.tab-content').hide();
-		this.$tabs.eq( that.currentIdx ).fadeOut( 'slow', function() {
-			that.$tabs.eq( index ).fadeIn( 'slow' ).addClass('selected');
-		} ).removeClass('selected');
-	} else {
-		this.$tabs.removeClass('selected').hide();
-		this.$tabs.eq( index ).show().addClass('selected');
-	}
+		this.$buttons.each( function() {
+			$( this ).on( that.eventType, function( e ) {
+				var clickedIdx = $.inArray( this, that.$buttons );
+				if( clickedIdx !== that.currentIdx ) {
+					that.switchTab( clickedIdx );
+					e.preventDefault();	
+				}
+			} );
+		} );
 
-	if( this.stateful && $.cookie ) {
-		$.cookie( this.kiscookie, index );
-	}
+		this.$prev.on( that.eventType, function( e ) {
+			if( that.currentIdx > 0 ) {
+				that.switchTab( that.currentIdx - 1 );
+			} else if( that.circular ) {
+				that.switchTab( that.$tabs.length - 1 );
+			}
+			e.preventDefault();
+		} );
 
-	this.currentIdx = index;
-	return false;
-};
+		this.$next.on( that.eventType, function( e ) {
+			if( that.currentIdx < that.$tabs.length - 1 ) {
+				that.switchTab( that.currentIdx + 1 );
+			} else if( that.circular ) {
+				that.switchTab( 0 );
+			}
+			e.preventDefault();
+		} );
+	};
+
+	KISCODE.TabSwitcher.prototype.switchTab = function( index ) {
+		// Handle tab highlighting
+		this.$buttons.eq( this.currentIdx ).removeClass( 'highlight' );
+		this.$buttons.eq( index ).addClass( 'highlight' );
+
+		// Handle content switching
+		if( this.animate ) {
+			var that = this;
+			this.$tabs.eq( that.currentIdx ).siblings('.tab-content').hide();
+			this.$tabs.eq( that.currentIdx ).fadeOut( 'slow', function() {
+				that.$tabs.eq( index ).fadeIn( 'slow' ).addClass('selected');
+			} ).removeClass('selected');
+		} else {
+			this.$tabs.removeClass('selected').hide();
+			this.$tabs.eq( index ).show().addClass('selected');
+		}
+
+		if( this.stateful && $.cookie ) {
+			$.cookie( this.kiscookie, index );
+		}
+
+		this.currentIdx = index;
+		return false;
+	};
+
+})(jQuery);
